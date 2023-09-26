1112d2d3e7798a5572fc279b1813a7605563469a
chrisholmes1128 <36671471+chrisholmes1128@users.noreply.github.com>
2023-09-26 19:04:49 -0400

    mobile fixes for skills page

--- portfolio-ch/src/components/Page/Page.tsx
+++ portfolio-ch/src/components/Page/Page.tsx
@@ -2,8 +2,17 @@ import { Box } from "@mui/material";
 import { isCurrentDeviceMobile } from "../../utils/isCurrentDeviceMobile";
 
 function Page({ children }: any) {
+  const isMobile = isCurrentDeviceMobile();
+  const height = isMobile ? window.innerHeight - 100 : "100%";
+
   return (
-    <Box width="100%" mt={2} justifyContent="center">
+    <Box
+      width="100%"
+      mt={2}
+      justifyContent="center"
+      height={height}
+      sx={{ overflowY: "auto" }}
+    >
       {children}
     </Box>
   );
